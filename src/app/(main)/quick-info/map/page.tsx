7251e710e25e6b7c3fd4a980d10d0e2310c21841
Tanboon Tiengtut <147372532+tanboon@users.noreply.github.com>
2026-03-21 22:54:52 +0700

    fix(map) : coming soon map page

---
+++ src/app/(main)/quick-info/map/page.tsx
@@ -0,0 +1,9 @@
+import MapView from "@/sections/quick-info/map/view/map-view";
+
+export const metadata = {
+  title: "แผนผังคณะ",
+};
+
+export default function MapPage() {
+  return <MapView />;
+}
